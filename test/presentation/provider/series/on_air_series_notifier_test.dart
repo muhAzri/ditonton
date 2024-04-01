630d7fe0be55994a64ae5e09f6a6d4c0ef1a421c
Azri <muhammad.azri.f.s@gmail.com>
2024-04-01 08:47:32 +0700

    add-test

---
+++ test/presentation/provider/series/on_air_series_notifier_test.dart
@@ -0,0 +1,78 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/series.dart';
+import 'package:ditonton/domain/usecases/get_on_air_series.dart';
+import 'package:ditonton/presentation/provider/series/on_air_series_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'on_air_series_notifier_test.mocks.dart';
+
+@GenerateMocks([GetOnAirSeries])
+void main() {
+  late MockGetOnAirSeries mockGetOnAirSeries;
+  late OnAirSeriesNotifier notifier;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockGetOnAirSeries = MockGetOnAirSeries();
+    notifier = OnAirSeriesNotifier(getOnAirSeries: mockGetOnAirSeries)
+      ..addListener(() {
+        listenerCallCount++;
+      });
+  });
+
+  const tSeries = Series(
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalName: 'originalName',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    name: 'name',
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tSeriesList = <Series>[tSeries];
+
+  test('should change state to loading when usecase is called', () async {
+    // arrange
+    when(mockGetOnAirSeries.execute())
+        .thenAnswer((_) async => Right(tSeriesList));
+    // act
+    notifier.fetchOnAirSeries();
+    // assert
+    expect(notifier.state, RequestState.Loading);
+    expect(listenerCallCount, 1);
+  });
+
+  test('should change series data when data is gotten successfully', () async {
+    // arrange
+    when(mockGetOnAirSeries.execute())
+        .thenAnswer((_) async => Right(tSeriesList));
+    // act
+    await notifier.fetchOnAirSeries();
+    // assert
+    expect(notifier.state, RequestState.Loaded);
+    expect(notifier.series, tSeriesList);
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return error when data is unsuccessful', () async {
+    // arrange
+    when(mockGetOnAirSeries.execute())
+        .thenAnswer((_) async => const Left(ServerFailure('Server Failure')));
+    // act
+    await notifier.fetchOnAirSeries();
+    // assert
+    expect(notifier.state, RequestState.Error);
+    expect(notifier.message, 'Server Failure');
+    expect(listenerCallCount, 2);
+  });
+}
