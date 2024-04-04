5a55d28ef099a0b8d4e6a01fd4fdea0f812117b3
Azri <muhammad.azri.f.s@gmail.com>
2024-04-04 23:02:03 +0700

    config(firebase):add-crashlytics-and-analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,33 @@
 import 'package:core/presentation/app.dart';
 import 'package:core/injector.dart' as di;
+import 'package:ditonton/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await setUpFirebase();
+  await setUpCrashlytics();
 
   await di.init();
   runApp(const MyApp());
+}
+
+Future<void> setUpFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
+
+Future<void> setUpCrashlytics() async {
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 }
